2cdfcd1b177af2e69231ab0267813d19ad6987b6
Amin Rezaeeyan <rezaeeyanamin@gmail.com>
2024-12-09 17:32:40 +0330

    fix: fix a bug in the ClockDivider The clk_out signal didn't have any initial value

--- rtl/modules/ClockDivider.v
+++ rtl/modules/ClockDivider.v
@@ -1,13 +1,20 @@
-module ClockDivider( 
+module ClockDivider (
     input clk_in,
     output reg clk_out
 );
+    parameter INPUT_CLOCK_FREQ = 40_000_000 // Default input clock frequency: 40 MHz
+    parameter DIVISOR = INPUT_CLOCK_FREQ / 2; // Default divisor for 1 Hz output
+
     reg [25:0] counter = 0;
 
+    initial begin
+        clk_out = 0;
+    end
+
     always @(posedge clk_in) begin
-        if (counter == 10000000 - 1) begin
+        if (counter == DIVISOR - 1) begin
             counter <= 0;
-            clk_out <= ~clk_out;  // Toggle clk_out
+            clk_out <= ~clk_out;
         end else begin
             counter <= counter + 1;
         end

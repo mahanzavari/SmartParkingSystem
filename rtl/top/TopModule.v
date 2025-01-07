a51dcb22973a9bc2e8d46fa9523bbdb07fe8591d
Amin Rezaeeyan <rezaeeyanamin@gmail.com>
2025-01-07 23:39:41 +0330

    final top module without door and full light(should be tested)

--- rtl/top/TopModule.v
+++ rtl/top/TopModule.v
@@ -14,80 +14,68 @@ module TopModule(
 );
 
 //     <------<< Internal wires >>------>
-
-//     Debounced wires
     wire debounced_entry_signal;
     wire debounced_exit_signal;
     wire [1:0] debounced_exit_location;
-//
-    // Door and full signals
-//    wire door_open_signal;
-//    wire lot_full_signal;
-//
-    // Clock division outputs
+
+    wire door_open_signal;
+    wire lot_full_signal;
+
     wire clk_1Hz;
     wire clk_2Hz;
-	 wire clk_500Hz;
+    wire clk_50Hz;
+	wire clk_500Hz;
 
-    // Parking status
-//    wire [3:0] parking_state_wire;
     wire [2:0] parking_capacity;
     wire [1:0] best_slot;
-//
+
     // <------<< Helper modules >>------>
 
-    // Clock Divider for generating 1Hz and 2Hz clocks
     ClockDivider clock_divider (
         .clk(clk),
         .reset(reset),
         .clk_1Hz(clk_1Hz),
         .clk_2Hz(clk_2Hz),
-		  .clk_500Hz(clk_500Hz)
+        .clk_50Hz(clk_50Hz),
+		.clk_500Hz(clk_500Hz)
     );
 
-   // Debouncer for entry signal
-Debouncer entry_debouncer (
-    .clk(clk),
-    .reset(reset),
-    .noisy_signal(~entry_sensor),
-    .stable_signal(debounced_entry_signal)
-);
+    Debouncer entry_debouncer (
+        .clk(clk_50Hz),
+        .reset(reset),
+        .button(~entry_sensor),
+        .debounced_pulse(debounced_entry_signal)
+    );
 
-// Debouncer for exit signal
-Debouncer exit_debouncer (
-    .clk(clk),
-    .reset(reset),
-    .noisy_signal(~exit_sensor),
-    .stable_signal(debounced_exit_signal)
-);
+    Debouncer exit_debouncer (
+        .clk(clk_50Hz),
+        .reset(reset),
+        .button(~exit_sensor),
+        .debounced_pulse(debounced_exit_signal)
+    );
 
-// Debouncing each bit of the `exit_location` signal
-wire debounced_exit_location_bit0;
-wire debounced_exit_location_bit1;
+    // Debouncing each bit of the `exit_location` signal
+    wire debounced_exit_location_bit0;
+    wire debounced_exit_location_bit1;
 
-Debouncer location_debouncer_bit0 (
-    .clk(clk),
-    .reset(reset),
-    .noisy_signal(exit_location[0]),
-    .stable_signal(debounced_exit_location_bit0)
-);
+    Debouncer location_debouncer_bit0 (
+        .clk(clk_50Hz),
+        .reset(reset),
+        .button(exit_location[0]),
+        .debounced_pulse(debounced_exit_location_bit0)
+    );
 
-Debouncer location_debouncer_bit1 (
-    .clk(clk),
-    .reset(reset),
-    .noisy_signal(exit_location[1]),
-    .stable_signal(debounced_exit_location_bit1)
-);
-
+    Debouncer location_debouncer_bit1 (
+        .clk(clk_50Hz),
+        .reset(reset),
+        .button(exit_location[1]),
+        .debounced_pulse(debounced_exit_location_bit1)
+    );
 
     // Combine the debounced bits back into a 2-bit signal
      assign debounced_exit_location = {debounced_exit_location_bit1, debounced_exit_location_bit0};
 
     // <------<< FSM Module for Parking System >>------>
-	 
-	 wire lot_full_signal;
-	 wire door_open_signal;
-	
 	 
     ParkingFSM parking_fsm (
         .clk(clk),
@@ -152,33 +140,17 @@ Debouncer location_debouncer_bit1 (
 //            end
 //        end
 //    end
-	 
-	 
-
-//    initial begin
-//        door_open_timer = 4'b0000;
-//        door_active = 1'b0;
-//        door_status_light = 1'b0;
-//        full_blink_counter = 2'b00;
-//        full_blink_active = 1'b0;
-//        lot_full_light = 1'b0;
-//    end
-//
-//
 
 //     <------<< Seven-Segment Display >>------>
     SevenSegmentDisplay seven_segment_display (
         .clk(clk_500Hz),
         .reset(reset),
-        .digit_0((parking_capacity == 3'b000) ? 4'b0101 :{2'b00,best_slot}),
+        .digit_0((parking_capacity == 3'b000) ? 4'b0101 : {2'b00,best_slot}),
         .digit_1((parking_capacity == 3'b000) ? 4'b0101 : 4'b0000),
         .digit_2({1'b0,parking_capacity}),
         .digit_3(4'b0000),
         .seg_out(seg_out),
         .anode(anode)
     );
-//
-    // Output assignments
-//    assign parking_state = parking_state_wire;
 
 endmodule
